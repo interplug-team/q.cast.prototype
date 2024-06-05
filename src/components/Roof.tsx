96996597e93723b22aa6771f0bd6255070328df9
hyojun.choi <chlgkwk123@nate.com>
2024-06-05 18:34:39 +0900

    기본기능 구현

--- src/components/Roof.tsx
+++ src/components/Roof.tsx
@@ -1,14 +1,16 @@
 import { fabric } from 'fabric'
 import { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Roof() {
   const [canvas, setCanvas] = useState<fabric.Canvas>()
+  const [isLocked, setIsLocked] = useState(false)
+  const [history, setHistory] = useState<fabric.Object[] | undefined>([])
 
   useEffect(() => {
     const c = new fabric.Canvas('canvas', {
       height: 800,
       width: 800,
-      backgroundColor: 'black',
     })
 
     // settings for all canvas in the app
@@ -29,11 +31,157 @@ export default function Roof() {
     const rect = new fabric.Rect({
       height: 200,
       width: 200,
-      stroke: '#2BEBC8',
+      top: 10,
+      left: 10,
+      stroke: randomColor(),
+      fill: 'white',
+      name: uuidv4(),
     })
-    canvas?.add(rect)
+
+    createFigure(rect)
+  }
+
+  const addHorizontalLine = (canvas?: fabric.Canvas) => {
+    /**
+     * 시작X,시작Y,도착X,도착Y 좌표
+     */
+    const horizontalLine = new fabric.Line([20, 20, 100, 20], {
+      name: uuidv4(),
+      stroke: randomColor(),
+      strokeWidth: 3,
+      selectable: true,
+    })
+
+    createFigure(horizontalLine)
+  }
+
+  const addVerticalLine = (canvas?: fabric.Canvas) => {
+    const verticalLine = new fabric.Line([10, 10, 10, 100], {
+      name: uuidv4(),
+      stroke: randomColor(),
+      strokeWidth: 3,
+      selectable: true,
+    })
+
+    createFigure(verticalLine)
+  }
+
+  const addTriangle = (canvas?: fabric.Canvas) => {
+    const triangle = new fabric.Triangle({
+      name: uuidv4(),
+      top: 50,
+      left: 50,
+      width: 100,
+      stroke: randomColor(),
+      strokeWidth: 3,
+    })
+
+    createFigure(triangle)
+  }
+
+  const createFigure = (figure: any) => {
+    canvas?.add(figure)
     canvas?.requestRenderAll()
   }
+
+  const handleClear = (canvas?: fabric.Canvas) => {
+    canvas?.clear()
+  }
+
+  const handleCopyFigure = (canvas?: fabric.Canvas) => {
+    const targetObj = canvas?.getActiveObject()
+    if (!targetObj) {
+      alert('복사할 대상을 선택해주세요.')
+      return
+    }
+
+    targetObj.clone((cloned: fabric.Object) => {
+      cloned.left! += 10
+      cloned.top! += 10
+      canvas?.add(cloned)
+    })
+  }
+
+  const handleDelete = (canvas?: fabric.Canvas) => {
+    const targetObj = canvas?.getActiveObject()
+    if (!targetObj) {
+      alert('삭제할 대상을 선택해주세요.')
+      return
+    }
+    canvas?.remove(targetObj)
+  }
+
+  const handleSave = (canvas?: fabric.Canvas) => {
+    const objects = canvas?.getObjects()
+
+    if (objects?.length === 0) {
+      alert('저장할 대상이 없습니다.')
+      return
+    }
+    const jsonStr = JSON.stringify(canvas)
+    localStorage.setItem('canvas', jsonStr)
+    canvas?.clear()
+  }
+
+  const handlePaste = (canvas?: fabric.Canvas) => {
+    const jsonStr = localStorage.getItem('canvas')
+    if (!jsonStr) {
+      alert('붙여넣기 할 대상이 없습니다.')
+      return
+    }
+
+    canvas?.loadFromJSON(JSON.parse(jsonStr), () => {
+      localStorage.removeItem('canvas')
+      console.log('paste done')
+    })
+  }
+
+  const randomColor = () => {
+    return '#' + Math.round(Math.random() * 0xffffff).toString(16)
+  }
+
+  const handleUndo = () => {
+    if (canvas) {
+      if (canvas._objects.length > 0) {
+        const poppedObject = canvas._objects.pop()
+        setHistory((prev: fabric.Object[] | undefined) => {
+          if (prev === undefined) {
+            return poppedObject ? [poppedObject] : []
+          }
+          return poppedObject ? [...prev, poppedObject] : prev
+        })
+        canvas.renderAll()
+      }
+    }
+  }
+
+  const handleRedo = () => {
+    if (canvas && history) {
+      if (history.length > 0) {
+        setIsLocked(true)
+        canvas.add(history[history.length - 1])
+        const newHistory = history.slice(0, -1)
+        setHistory(newHistory)
+      }
+    }
+  }
+
+  const onChange = () => {
+    if (!isLocked) {
+      setHistory([])
+    }
+    setIsLocked(false)
+  }
+
+  useEffect(() => {
+    if (!canvas) {
+      return
+    }
+
+    canvas.on('object:added', onChange)
+    canvas.on('object:modified', onChange)
+    canvas.on('object:removed', onChange)
+  }, [canvas])
 
   return (
     <>
@@ -46,48 +194,89 @@ export default function Roof() {
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-blue-500 text-white"
-          onClick={() => {}}
+          onClick={() => {
+            addHorizontalLine(canvas)
+          }}
         >
           ADD HORIZONTAL LINE
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-blue-500 text-white"
-          onClick={() => {}}
+          onClick={() => {
+            addVerticalLine(canvas)
+          }}
         >
           ADD VERTICALITY LINE
         </button>
         <button
+          className="w-30 mx-2 p-2 rounded bg-blue-500 text-white"
+          onClick={() => {
+            addTriangle(canvas)
+          }}
+        >
+          ADD TRIANGLE
+        </button>
+        <button
+          className="w-30 mx-2 p-2 rounded bg-black text-white"
+          onClick={() => {
+            handleCopyFigure(canvas)
+          }}
+        >
+          COPY FIGURE
+        </button>
+        <button
           className="w-30 mx-2 p-2 rounded bg-red-500 text-white"
-          onClick={() => {}}
+          onClick={() => {
+            handleDelete(canvas)
+          }}
+        >
+          DELETE
+        </button>
+        <button
+          className="w-30 mx-2 p-2 rounded bg-red-500 text-white"
+          onClick={() => {
+            handleClear(canvas)
+          }}
         >
           CLEAR
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-green-500 text-white"
-          onClick={() => {}}
+          onClick={() => {
+            handleUndo()
+          }}
         >
           UNDO
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-green-300 text-white"
-          onClick={() => {}}
+          onClick={() => {
+            handleRedo()
+          }}
         >
           REDO
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-black text-white"
-          onClick={() => {}}
+          onClick={() => {
+            handleSave(canvas)
+          }}
         >
           저장
         </button>
         <button
           className="w-30 mx-2 p-2 rounded bg-black text-white"
-          onClick={() => {}}
+          onClick={() => {
+            handlePaste(canvas)
+          }}
         >
           붙여넣기
         </button>
       </div>
-      <div className="flex justify-center">
+      <div
+        className="flex justify-center"
+        style={{ border: '1px solid', width: 800, height: 800, margin: 'auto' }}
+      >
         <canvas id="canvas" />
       </div>
     </>
